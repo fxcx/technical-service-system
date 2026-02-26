4d92ba9a61a4e33078fb5cb4f0016c52cb5de9c5
taller <tallerdecalderasoficial@gmail.com>
2026-02-26 19:13:31 +0000

    coneccion a la base de datos

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,10 +3,7 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
   datasource: {
-    url: env("DATABASE_URL_DEV"),
+    url: env("DIRECT_URL"),
   },
 });
